68b0e28e152b288acc630e3257b2eb55beebfc36
= <szopen1290@gmail.com>
2024-06-08 18:44:56 +0200

    added finding user by phone number

--- Machine/machine_backend/app/views.py
+++ Machine/machine_backend/app/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import MachineEvent, BottlesInAutomat, BottlesCollectionHistory, Address, EventType, User, UserBalance, UserBottleDetails, Machine
 from .serializers import (MachineEventSerializer, BottlesInAutomatSerializer,
                           BottlesCollectionHistorySerializer, AddressSerializer,
@@ -28,6 +30,17 @@ class EventTypeViewSet(viewsets.ModelViewSet):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @action(detail=False, methods=['get'], url_path='find-by-telephone')
+    def find_by_telephone(self, request):
+        phone = request.query_params.get('phone', None)
+        if phone is not None:
+            user = User.objects.filter(phone=phone).first()
+            if user is not None:
+                serializer = self.get_serializer(user)
+                return Response(serializer.data)
+            return Response({'detail': 'User not found'}, status=404)
+        return Response({'detail': 'Phone number is required'}, status=400)
 
 class UserBalanceViewSet(viewsets.ModelViewSet):
     queryset = UserBalance.objects.all()
