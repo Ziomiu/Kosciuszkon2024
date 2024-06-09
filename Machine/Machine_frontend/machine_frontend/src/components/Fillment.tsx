175faaeb31b14edede66c632204c6f5215294249
mateuszlampert <mateuszlampert@gmail.com>
2024-06-09 04:02:18 +0200

    implemnted view of machine fillment details

---
+++ Machine/Machine_frontend/machine_frontend/src/components/Fillment.tsx
@@ -0,0 +1,38 @@
+import { FaBottleWater } from "react-icons/fa6";
+
+const iconColor = (id: number) => {
+    if (id < 4) {
+        return "green"
+    } else if (id < 8) {
+        return "orange"
+    } else {
+        return "red"
+    }
+}
+
+const bottlesBar = (percentage: number) => {
+    return <>
+        <div>
+            {
+                [...Array(Math.round(percentage * 10))].map((_, i)=>
+                    <FaBottleWater color={iconColor(i)} fontSize="50px" />
+                )
+            }
+            {
+                [...Array(Math.floor(10 - percentage * 10))].map(_ =>
+                    <FaBottleWater color="transparent" stroke="grey" strokeWidth="20px" fontSize="50px" />
+                )
+            }
+        </div>
+    </>
+}
+
+export default function Fillment(params: { percentage: number }) {
+    return <>
+        
+        
+        <div style={{display: "flex", justifyContent: "center"}}>
+            {bottlesBar(params.percentage)}
+        </div>
+    </>
+}
